1ba79ae4aa679f4ed1397cacf4d9d27d322ffa03
junsang-ee <dkwnstkddk@gmail.com>
2023-05-03 13:39:13 +0900

    add LoggerStatus enum file in constantPackage, add content LoggerEntity

---
+++ src/main/java/com/logger/web/constant/LoggerStatus.java
@@ -0,0 +1,5 @@
+package com.logger.web.constant;
+
+public enum LoggerStatus {
+    WAITING, MOVEMENT, ARRIVAL
+}
--- src/main/java/com/logger/web/model/entity/LoggerEntity.java
+++ src/main/java/com/logger/web/model/entity/LoggerEntity.java
@@ -1,5 +1,6 @@
 package com.logger.web.model.entity;
 
+import com.logger.web.constant.LoggerStatus;
 import com.logger.web.model.entity.common.CreationLocalDateTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
@@ -14,5 +19,28 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @Entity(name = "logger")
 public class LoggerEntity extends CreationLocalDateTimeEntity {
+    private String name;
+
+    private String macAddress;
+
+    private int serialNumber;
+
+    private int battery;
+
+    @Enumerated(EnumType.STRING)
+    private LoggerStatus status;
+
+    private double firmware;
+
+    private LocalDateTime connectedAt;
+
+    @PrePersist
+    public void onPrevisionPersist() {
+        this.setBattery(100);
+        this.setStatus(LoggerStatus.valueOf(LoggerStatus.WAITING.name()));
+        this.connectedAt = null;
+    }
+
+
 
 }
