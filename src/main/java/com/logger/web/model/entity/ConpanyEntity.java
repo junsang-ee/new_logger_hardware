fa13025740f9a14478ca749a9d5e5b1d3242e719
junsang-ee <dkwnstkddk@gmail.com>
2023-05-03 14:12:52 +0900

    create CompanyEntity

---
+++ src/main/java/com/logger/web/model/entity/ConpanyEntity.java
@@ -0,0 +1,26 @@
+package com.logger.web.model.entity;
+
+import com.logger.web.model.entity.common.CreationLocalDateTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "company")
+public class ConpanyEntity extends CreationLocalDateTimeEntity {
+    private String name;
+
+    private String departmentName;
+
+    private int zipCode;
+
+    private String address;
+
+    private String detailAddress;
+
+    private String representativeNumber;
+
+    private String businessNumber;
+}
