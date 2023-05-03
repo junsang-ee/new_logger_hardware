5353202c57889f4740240cbe9aae6cc3161e0137
junsang-ee <dkwnstkddk@gmail.com>
2023-05-03 13:43:29 +0900

    create appPushEntity, ReceptionConsentType Constant

---
+++ src/main/java/com/logger/web/constant/ReceptionConsentType.java
@@ -0,0 +1,5 @@
+package com.logger.web.constant;
+
+public enum ReceptionConsentType {
+    AGREEMENT, DISAGREEMENT
+}
---
+++ src/main/java/com/logger/web/model/entity/AppPushEntity.java
@@ -0,0 +1,35 @@
+package com.logger.web.model.entity;
+
+import com.logger.web.constant.ReceptionConsentType;
+import com.logger.web.model.entity.common.CreationLocalDateTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.PrePersist;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "app_push")
+public class AppPushEntity extends CreationLocalDateTimeEntity {
+
+    @Enumerated(EnumType.STRING)
+    private ReceptionConsentType logisticsStatus;
+
+    @Enumerated(EnumType.STRING)
+    private ReceptionConsentType eventOccurs;
+
+    @Enumerated(EnumType.STRING)
+    private ReceptionConsentType nightReception;
+
+    @PrePersist
+    private void prePersist() {
+        this.logisticsStatus = ReceptionConsentType.AGREEMENT;
+        this.eventOccurs = ReceptionConsentType.AGREEMENT;
+        this.nightReception = ReceptionConsentType.AGREEMENT;
+    }
+}
